7a3b3ab8372caad5a26c4508f21fb1891748c59e
Zheng Kai <zhengkai@gmail.com>
2026-04-15 17:19:56 +0800

    up

--- module/desktop.nix
+++ module/desktop.nix
@@ -4,10 +4,6 @@ let
   isDesktop = config.services.xserver.enable || config.programs.hyprland.enable;
 in
 {
-  imports = lib.mkIf isDesktop [
-    ./font.nix
-  ];
-
   config = lib.mkIf isDesktop {
     environment.systemPackages = with pkgs; [
       google-chrome
--- module/font.nix
+++ module/font.nix
@@ -1,11 +1,25 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
-  config = lib.mkIf config.services.xserver.enable || config.programs.hyprland.enable {
-    environment.systemPackages = with pkgs; [
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      source-han-sans
+      source-han-serif
       source-sans
       source-serif
       jetbrains-mono
     ];
+
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Source Han Serif" "Noto Serif CJK SC" ];
+        sansSerif = [ "Source Han Sans" "Noto Sans CJK SC" ];
+        monospace = [ "Noto Sans Mono" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
   };
 }
